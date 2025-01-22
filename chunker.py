4854c8c0de46e98d181b67b05a4327fbaeda4cf4
eamongie <112226104+eamongie@users.noreply.github.com>
2025-01-21 19:38:10 -0500

    Update chunker.py

--- chunker.py
+++ chunker.py
@@ -7,7 +7,10 @@ COLORS = {
     "動詞": "green",  # Verb
     "形容詞": "red",  # Adjective
     "助詞": "purple",  # Particle
-    "その他": "gray",  # Default
+    "助動詞": "orange",  # Auxiliary verb
+    "副詞": "teal",  # Adverb
+    "感動詞": "pink",  # Interjection
+    "その他": "gray",  # Default for unknown or other types
 }
 
 def chunk_sentence(sentence):
